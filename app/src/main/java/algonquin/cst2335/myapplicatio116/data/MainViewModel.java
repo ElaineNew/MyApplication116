f2645026b5cf7e5beef2457b64260bf7e9080285
Jiaying Qiu <elaineqiu0531@gmail.com>
2024-01-17 12:39:05 -0500

    "Using lambda and ViewModel"

--- app/src/main/java/algonquin/cst2335/myapplicatio116/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2335/myapplicatio116/data/MainViewModel.java
@@ -1,4 +1,9 @@
 package algonquin.cst2335.myapplicatio116.data;
 
-public class MainViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    public MutableLiveData<String> editString = new MutableLiveData<>();
+
 }
--- app/src/main/java/algonquin/cst2335/myapplicatio116/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/myapplicatio116/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package algonquin.cst2335.myapplicatio116.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,32 +9,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import algonquin.cst2335.myapplicatio116.data.MainViewModel;
 import algonquin.cst2335.myapplicatio116.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding variableBinding;
+    private MainViewModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//
-//        setContentView(R.layout.activity_main);
-//        TextView mytext = findViewById(R.id.textview);
+        model = new ViewModelProvider(this).get(MainViewModel.class);
+        model.editString.observe(this, s-> {
+            variableBinding.mytext.setText("Your edit text has " + s);
+        });
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
-//        TextView mytext = findViewById(R.id.textview);
-//        Button btn = findViewById(R.id.mybutton);
-//        EditText myedit = findViewById(R.id.myedittext);
-        TextView mytext = variableBinding.textview;
-        Button btn = variableBinding.mybutton;
-        EditText myedit = variableBinding.myedittext;
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String editString = myedit.getText().toString();
-                mytext.setText("Your edit text has:" + editString);
-            }
+        variableBinding.mybutton.setOnClickListener(click -> {
+            model.editString.postValue(variableBinding.myedittext.getText().toString());
         });
     }
 
