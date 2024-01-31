14603135fd6793fb267e54fac4c8949fc51b06f5
Jiaying Qiu <elaineqiu0531@gmail.com>
2024-01-31 18:28:54 -0500

    "Add new page in Week4"

--- app/src/main/java/algonquin/cst2335/myapplicatio116/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/myapplicatio116/MainActivity.java
@@ -2,7 +2,10 @@ package algonquin.cst2335.myapplicatio116;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        Button loginButton = findViewById(R.id.loginbutton);
+
+        Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+        loginButton.setOnClickListener(clk->{
+            startActivity(nextPage);
+        });
     }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( "MainActivity", "In onStart() - The application is now visibe on screen" );
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( "MainActivity", "In onResume() - The application is now responding to user input" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( "MainActivity", "In onPause() - The application no longer responds to user input" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w( "MainActivity", "In onStop() -The application is no longer visible." );
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w( "MainActivity", "In onDestroy() - Any memory used by the application is freed" );
+    }
+
+
+
 }
